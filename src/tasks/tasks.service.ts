99a520e1b11da5ffe3fe8a07b2c833994c1013e4
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-19 16:08:21 +0200

    📦 NEW:  add search/filter

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,8 @@
+import * as uuid from 'uuid/v1'
 import { Injectable } from '@nestjs/common'
 import { Task, TaskStatus } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
-import * as uuid from 'uuid/v1'
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 
 @Injectable()
 export class TasksService {
@@ -9,6 +10,20 @@ export class TasksService {
 
   getAllTasks(): Task[] {
     return this.tasks
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto
+    let tasks = this.getAllTasks()
+    if (status) {
+      tasks = tasks.filter(task => task.status === status)
+    }
+    if (search) {
+      tasks = tasks.filter(
+        task => task.title.includes(search) || task.description.includes(search)
+      )
+    }
+    return tasks
   }
 
   getTaskById(id: string): Task {
