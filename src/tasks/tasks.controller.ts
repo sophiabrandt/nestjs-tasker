1f1677ee1bc472f55598e611d702bb7df8f75996
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-19 15:49:28 +0200

    📦 NEW: get, update and delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Post, Body } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Delete,
+  Post,
+  Patch,
+  Body,
+  Param,
+} from '@nestjs/common'
 import { TasksService } from './tasks.service'
-import { Task } from './task.model'
+import { Task, TaskStatus } from './task.model'
 import { CreateTaskDto } from './dto/create-task.dto'
 
 @Controller('tasks')
@@ -12,8 +20,26 @@ export class TasksController {
     return this.tasksService.getAllTasks()
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id)
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(createTaskDto)
+  }
+
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status)
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id)
   }
 }
