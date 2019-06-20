68a2d023ae4be0d45cf7bb7eaf3360520a660a9d
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-20 21:50:39 +0200

    📦 NEW: add hashing to passwords

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,24 +1,31 @@
+import * as bcrypt from 'bcrypt'
 import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { Repository, EntityRepository } from 'typeorm'
-import { User } from './user.entity'
 import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+import { User } from './user.entity'
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+  async signUp (authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto
 
     const user = new User()
     user.username = username
-    user.password = password
+    user.salt = await bcrypt.genSalt()
+    user.password = await this.hashPassword(password, user.salt)
 
     try {
       await user.save()
     } catch (error) {
       // duplicate username
-      if (error.code === '23505')
+      if (error.code === '23505') {
         throw new ConflictException('Username already exists')
+      }
+      throw new InternalServerErrorException()
     }
-    throw new InternalServerErrorException()
+  }
+
+  private async hashPassword (password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt)
   }
 }
