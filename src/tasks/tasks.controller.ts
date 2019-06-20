85a3edcf2458a4cc8bec5dc5727c1a8603775a2d
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-20 13:28:01 +0200

    👌 IMPROVE: add task status update with task repository

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -41,13 +41,13 @@ export class TasksController {
     return this.tasksService.createTask(createTaskDto)
   }
 
-  // @Patch('/:id/status')
-  // updateTask(
-  //   @Param('id') id: string,
-  //   @Body('status', TaskStatusValidationPipe) status: TaskStatus
-  // ): Task {
-  //   return this.tasksService.updateTaskStatus(id, status)
-  // }
+  @Patch('/:id/status')
+  updateTask(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus
+  ): Promise<Task> {
+    return this.tasksService.updateTaskStatus(id, status)
+  }
 
   @Delete('/:id')
   deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
