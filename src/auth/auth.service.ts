6e5f618fd138d201459282f7b3c6731856cb51cb
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-20 20:15:55 +0200

    📦 NEW: add signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { InjectRepository } from '@nestjs/typeorm'
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { UserRepository } from './user.repository'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository
   ) {}
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.userRepository.signUp(authCredentialsDto)
+  }
 }
