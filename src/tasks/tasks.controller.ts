58515186e552b7f2ce3d1814bc060aa708b3b459
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-21 10:53:23 +0200

    📦 NEW: add auth guard for task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,9 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
+  UseGuards
 } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
 import { TasksService } from './tasks.service'
 import { TaskStatus } from './task-status.enum'
 import { CreateTaskDto } from './dto/create-task.dto'
@@ -19,27 +21,28 @@ import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe'
 import { Task } from './task.entity'
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
-  constructor(private tasksService: TasksService) {}
+  constructor (private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
+  getTasks (@Query(ValidationPipe) filterDto: GetTasksFilterDto) {
     return this.tasksService.getTasks(filterDto)
   }
 
   @Get('/:id')
-  getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+  getTaskById (@Param('id', ParseIntPipe) id: number): Promise<Task> {
     return this.tasksService.getTaskById(id)
   }
 
   @Post()
   @UsePipes(ValidationPipe)
-  createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+  createTask (@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.tasksService.createTask(createTaskDto)
   }
 
   @Patch('/:id/status')
-  updateTask(
+  updateTask (
     @Param('id', ParseIntPipe) id: number,
     @Body('status', TaskStatusValidationPipe) status: TaskStatus
   ): Promise<Task> {
@@ -47,7 +50,7 @@ export class TasksController {
   }
 
   @Delete('/:id')
-  deleteTask(@Param('id', ParseIntPipe) id: number): Promise<void> {
+  deleteTask (@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.tasksService.deleteTask(id)
   }
 }
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { TaskRepository } from './task.repository'
 import { TasksController } from './tasks.controller'
 import { TasksService } from './tasks.service'
+import { AuthModule } from '../auth/auth.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   controllers: [TasksController],
-  providers: [TasksService],
+  providers: [TasksService]
 })
 export class TasksModule {}
