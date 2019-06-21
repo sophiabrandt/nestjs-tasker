283755e7cae6244d5cb84cea3ed5bed6383580bd
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-21 08:26:54 +0200

    📦 NEW: add dotenv, passport, jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,6 @@
+require('dotenv').config()
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthController } from './auth.controller'
@@ -5,8 +8,15 @@ import { AuthService } from './auth.service'
 import { UserRepository } from './user.repository'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: 3600 }
+    }),
+    TypeOrmModule.forFeature([UserRepository])
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService]
 })
 export class AuthModule {}
