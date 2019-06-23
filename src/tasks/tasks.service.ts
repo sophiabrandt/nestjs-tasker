df3d9cb75ba78a507368b62ff04cf1f44e84cf36
Sophia Brandt <16630701+sophiabrandt@users.noreply.github.com>
2019-06-23 20:51:03 +0200

    📦 NEW: add relationship to entities

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,19 +5,20 @@ import { CreateTaskDto } from './dto/create-task.dto'
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto'
 import { TaskRepository } from './task.repository'
 import { Task } from './task.entity'
+import { User } from '../auth/user.entity'
 
 @Injectable()
 export class TasksService {
-  constructor(
+  constructor (
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository
   ) {}
 
-  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTasks (filterDto: GetTasksFilterDto): Promise<Task[]> {
     return this.taskRepository.getTasks(filterDto)
   }
 
-  async getTaskById(id: number): Promise<Task> {
+  async getTaskById (id: number): Promise<Task> {
     const found = await this.taskRepository.findOne(id)
     if (!found) {
       throw new NotFoundException(`Task with id '${id}' not found.`)
@@ -26,18 +27,18 @@ export class TasksService {
     return found
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask (createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto)
   }
 
-  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+  async updateTaskStatus (id: number, status: TaskStatus): Promise<Task> {
     const task = await this.getTaskById(id)
     task.status = status
     task.save()
     return task
   }
 
-  async deleteTask(id: number): Promise<void> {
+  async deleteTask (id: number): Promise<void> {
     const result = await this.taskRepository.delete(id)
 
     if (result.affected === 0) {
